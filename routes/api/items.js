2a250a4c3ff30bed86a1c87d36a65ae24e1b8f2a
Pooja patel <poojavpatel71@gmail.com>
2018-09-22 20:53:23 +0530

    post n delete req done

--- routes/api/items.js
+++ routes/api/items.js
@@ -4,10 +4,28 @@ const Item = require('../../models/Item.js');
 
 const router = express.Router();
 
-// @/api/items/  GET req
+// @/api/items/ | GET req | get all items
 router.get('/' ,async (req,res) => {
     const items = await Item.find().sort('name');
     res.send(items);
+});
+
+// @/api/items/ | POST req | create an item
+router.post('/',async (req,res) => {
+    const item = new Item({
+        name:req.body.name
+    })
+    const resitem = await item.save();
+    res.send(resitem);
+});
+
+// @/api/items/:name | DELETE req | delete an item
+router.delete('/:id',async (req,res) => {
+    const reqitem = await Item.findByIdAndRemove(req.params.id);
+    if(!reqitem){
+       return res.status(404).send("This item was not found");
+    }
+    res.send("sucessfully deleted item");
 });
 
 module.exports = router;
