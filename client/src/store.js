093545a947622912dec6e5e6e69f2de7452079c7
Pooja patel <poojavpatel71@gmail.com>
2018-11-24 13:12:24 +0530

    installed redux and react-redux|code file store.js

---
+++ client/src/store.js
@@ -0,0 +1,15 @@
+/* Entry point for redux */
+/* jshint esversion:6 */
+import { createStore, applyMiddleware, compose }from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer form './reducers/index.js';
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(rootReducer, initialState, 
+    compose( applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() )
+)
+
+export default store;
